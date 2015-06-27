fb4948ab88364db7a7bb2ab4a45ab7e59529d8d4
Quentin PANISSIER <contact@meshup.net>
2015-06-27 10:59:53 +0200

    Ajout des couleurs au manpage

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,6 +26,14 @@ set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
 set -x PATH $PATH $HOME/bin $HOME/.gem/ruby/2.2.0/bin
 
+set -xU LESS_TERMCAP_mb (printf "\e[01;31m")      # begin blinking
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")      # begin bold
+set -xU LESS_TERMCAP_me (printf "\e[0m")          # end mode
+set -xU LESS_TERMCAP_se (printf "\e[0m")          # end standout-mode
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
+set -xU LESS_TERMCAP_ue (printf "\e[0m")          # end underline
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
+
 alias grep='grep --color=auto'
 alias ls='ls --color=auto -lh'
 alias c='clear'
